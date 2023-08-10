cccb4d3a4f77aefa60933f27a4f4e10b0d649943
mrAdiletDev <adiletdj19@gmail.com>
2023-08-10 18:30:24 +0600

    uncomments apps in settings.py

--- config/settings.py
+++ config/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     'crispy_forms',
     "crispy_bootstrap5",
 
-    # 'hogwarts',
-    # 'posts',
+    'hogwarts',
+    'posts',
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
