5c7a678a5f516c2d475203f884d98fe4eac68e85
mrAdiletDev <adiletdj19@gmail.com>
2023-08-10 18:58:44 +0600

    add posts to config urls

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("example/", include("hogwarts.urls"))
+    path("example/", include("hogwarts.urls")),
+    path("posts/", include("posts.urls"))
 ]
