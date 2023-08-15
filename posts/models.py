6b8c878ef8bf2a5c716a05e850e3063201d1d05d
mrAdiletDev <adiletdj19@gmail.com>
2023-08-15 22:12:42 +0600

    remove posts from git 📦

--- posts/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    tags = models.CharField(max_length=255, help_text="write hashtags seperated by comma")
-    content = models.TextField(help_text="You can use markdown!")
-    created_at = models.DateTimeField(auto_now_add=True)
