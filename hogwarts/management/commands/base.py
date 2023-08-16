061b3104ed45927af0a3840ea3ee557e3fe3cc9e
mrAdiletDev <adiletdj19@gmail.com>
2023-08-16 16:28:01 +0600

    fix get views 🛠

--- hogwarts/management/commands/base.py
+++ hogwarts/management/commands/base.py
@@ -12,8 +12,9 @@ def get_app_config(app_name: str):
 
 
 def get_views_module(app_name: str):
+    module = apps.get_app_config(app_name).name
     try:
         # Import the views.py file dynamically
-        return importlib.import_module(f"{app_name}.views")
+        return importlib.import_module(f"{module}.views")
     except ModuleNotFoundError:
         raise CommandError(f"Views not found for app '{app_name}'.")
